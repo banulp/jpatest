0f30008ce95493b18d341c8cf5285a7fb3bffce0
banulp <banulp@ncsoft.com>
2022-03-25 15:55:17 +0900

    code api vr list

--- src/main/java/com/example/accessingdatamysql/ApiRepository.java
+++ src/main/java/com/example/accessingdatamysql/ApiRepository.java
@@ -2,5 +2,8 @@ package com.example.accessingdatamysql;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApiRepository extends JpaRepository<Api, Long> {
+    List<Api> findByCode(String code);
 }
--- src/main/java/com/example/accessingdatamysql/GdController.java
+++ src/main/java/com/example/accessingdatamysql/GdController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Controller	// This means that this class is a Controller
@@ -19,6 +20,12 @@ public class GdController {
 	@GetMapping(path="/allapi")
 	public @ResponseBody List<Api> init() {
 		List<Api> list = ar.findAll(); // (5)
+		return list;
+	}
+
+	@GetMapping(path="/allapi/{code}")
+	public @ResponseBody List<Api> codeApi(@PathVariable String code) {
+		List<Api> list = ar.findByCode(code); // (5)
 		return list;
 	}
 
